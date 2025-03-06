d7611d12669a749bb5586a523ee6f867e23bb847
Lang Hall <lmilton@heckgrammar.co.uk>
2025-03-06 18:30:03 +0000

    fixed up till cow and bull counting

--- WorkingKindof/Program.cs
+++ WorkingKindof/Program.cs
@@ -17,6 +17,7 @@ namespace CowBullGame
 
             while (!validNum) //loop runs until valid number is generated
             {
+                validNum=true; //assume the number is valid until we find a duplicate
                 CowAndBullNum = rand.Next(1023, 9876); //generates a random number between x and y (what is the highest number it can generate?) 
 
 
@@ -31,34 +32,57 @@ namespace CowBullGame
                     {
                         if (CowBullString[i] == CowBullString[j])
                         {
-                            repeat = true; //if any digits match, set repeat to true 
-                            break; //exit the inner loop if the duplicate was found 
+                            validNum = false; //if any digits match, set repeat to true 
+                             //exit the inner loop if the duplicate was found 
                         }
                     }
-                    if (repeat) break;  //Exit the outer loop as well if a duplicate was found 
+                 //Exit the outer loop as well if a duplicate was found 
                 }
-                validNum = !repeat; // if repeat is False, it means all digits are unique, so ValidNum becomes 'True' 
+               // if repeat is False, it means all digits are unique, so ValidNum becomes 'True' 
                 //stop the while loop - continue until a unique number is found 
             }
 
             Console.WriteLine("A valid 4 digit number has been generated!");
             string secretNumber = CowAndBullNum.ToString(); //converts the numbe to a string so we can check for repeats
-
-            Console.WriteLine("Enter a guess");
-            string guess = Console.ReadLine();
-            bool Valid = false;
+            string guess= "";
 
             while (!win) //loop runs until win set to true 
             {
+                bool ValidGuess = false;
                 Console.Write("Enter a 4 digit guess: "); //prompt the user to enter a guess
-                guess = Console.ReadLine();
-                guessNum++; //increment the guess number
-
-                if (!HasUniqueDigits(guess)) //check if the guess has unique digits
+                guess = Console.ReadLine(); //read the user's input
+                do 
                 {
-                    Console.WriteLine("Invalid guess, try again!"); //if not, print an error message and continue the loop
-                    continue;
-                }
+                    ValidGuess = true ;
+                    if (guess.Length != 4) //check if the guess is 4 digits long
+                    {
+                        ValidGuess = false;
+                    }
+                    for (int i = 0; i < 2; i++) //loop through each digit in the string
+                    {
+                        for (int j = i + 1; j < 4; j++) //compare digit I with all digits after
+                        {
+                            if (guess[i] == guess[j]) //if any digits match, return false
+                            {
+                                ValidGuess =  false;
+                            }
+                        }
+                    }
+                    if (!ValidGuess)
+                    {
+                        Console.WriteLine("Invalid guess, too long or repeating numbers!");
+                        Console.Write("Enter a 4 digit guess: "); //prompt the user to enter a guess
+                        guess = Console.ReadLine(); //read the user's input
+                    }
+                }while (!ValidGuess);
+                guessNum++; //increment the guess number
+                Console.WriteLine("test for cowa n bulls");
+
+                //if (!HasUniqueDigits(guess)) //check if the guess has unique digits
+                //{
+                //    Console.WriteLine("Invalid guess, too long or repeating numbers!");
+                //    //if not, print an error message and continue the loop
+                //}
 
                 if (guess == secretNumber) //check if the guess is correct
                 {
@@ -69,32 +93,10 @@ namespace CowBullGame
                 else
                 {
                     Console.WriteLine("Incorrect Guess try again");
+                    //do cows and bull count here
 
                 }
 
-
-                static bool HasUniqueDigits(string str) //method to check if the guess has unique digits
-                {
-                    if (str.Length != 4) //check if the guess is 4 digits long
-                    {
-                        return false; //if not, return false
-                    }
-
-                    for (int i = 0; i < 3; i++) //loop through each digit in the string
-                    {
-                        for (int j = i + 1; j < 4; j++) //compare digit I with all digits after
-                        {
-                            if (str[i] == str[j]) //if any digits match, return false
-                            {
-                                return false;
-                            }
-                        }
-                    }
-
-
-                    return true; //if no duplicates were found, return true
-
-                }
             }
 
         }
